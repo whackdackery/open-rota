b5ffeb55c2e3ad1268c9b37c36cb424c7e7ab9e1
chrissearle <chris.searle@brightpearl.com>
2021-05-03 18:01:56 +0100

    add roles flyway migrations

--- src/main/resources/db/migration/V0_1__users.sql
+++ src/main/resources/db/migration/V0_1__users.sql
@@ -14,3 +14,39 @@ create table if not exists users
 INSERT INTO users (username, email) VALUES ('superadmin', 'superadmin@email.com');
 INSERT INTO users (username, email) VALUES ('admin', 'admin@email.com');
 INSERT INTO users (username, email) VALUES ('enduser', 'enduser@email.com');
+
+create table if not exists roles
+(
+    id int auto_increment primary key,
+    code varchar(128) not null,
+    constraint code
+        unique (code),
+    constraint roles_id_uindex
+        unique (id)
+);
+
+INSERT INTO roles (id, code) VALUES (1, 'SUPER_ADMIN');
+INSERT INTO roles (id, code) VALUES (2, 'ADMIN');
+INSERT INTO roles (id, code) VALUES (3, 'EDITOR');
+INSERT INTO roles (id, code) VALUES (4, 'SCHEDULER');
+INSERT INTO roles (id, code) VALUES (5, 'END_USER');
+
+create table if not exists user_roles
+(
+    user_id int not null,
+    role_id int null,
+    constraint user_roles_pk
+        unique (user_id, role_id),
+    constraint user_roles_roles_id_fk
+        foreign key (role_id) references roles (id)
+            on delete cascade,
+    constraint user_roles_users_id_fk
+        foreign key (user_id) references users (id)
+            on delete cascade
+);
+
+INSERT INTO user_roles (user_id, role_id) VALUES (1, 1);
+INSERT INTO user_roles (user_id, role_id) VALUES (1, 3);
+INSERT INTO user_roles (user_id, role_id) VALUES (2, 2);
+INSERT INTO user_roles (user_id, role_id) VALUES (2, 5);
+INSERT INTO user_roles (user_id, role_id) VALUES (3, 5);
